60278ff936bbaf6de3e7f366dc21bf004f4e140a
Gab Martin <7290@holbertonstudents.com>
2023-10-16 06:51:59 -0700

    Numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
