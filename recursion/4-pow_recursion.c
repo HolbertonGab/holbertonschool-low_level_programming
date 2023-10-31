9db09353056bc88b8559b0d359655aae793fe104
Gab Martin <7290@holbertonstudents.com>
2023-10-31 03:40:37 -0700

    Write a function that returns the value of x raised to the power of y.

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -3,6 +3,8 @@
  * _pow_recursion - func that returns the value of x raised to the power of y.
  * @x: value x.
  * @y: value y.
+ *
+ * Return: x raised by y.
  */
 int _pow_recursion(int x, int y)
 {
