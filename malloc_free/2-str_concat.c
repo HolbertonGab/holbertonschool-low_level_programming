6f85a81d25fdceb1c623d2dbeb53bdfd3c5bb1e3
Gab Martin <7290@holbertonstudents.com>
2023-11-06 05:37:01 -0800

    Write a function that concatenates two strings with malloc.

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -17,11 +17,7 @@ char *str_concat(char *s1, char *s2)
 	int i;
 	char *result;
 
-	if (s1 == NULL)
-	{
-		return (NULL);
-	}
-	if (s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 	{
 		return (NULL);
 	}
