6da6637cc2651c5ee7d727a8aaae61c229e2add1
HolbertonGab <7290@holbertonstudents.com>
2023-11-17 14:58:59 +0000

    function that prints anything

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -66,18 +66,6 @@ void print_all(const char * const format, ...)
 	};
 
 	va_start(ap, format);
-	int i = 0
-
-		for (i = 0; types[i].type; ++i)
-		{
-			if (types[i].type == *format)
-			{
-				types[i].func(ap);
-				printf(", ");
-				break;
-			}
-		}
-		++format;
 	va_end(ap);
 	printf("\n");
 }
